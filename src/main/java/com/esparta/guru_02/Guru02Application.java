a6b3d3beb7f20b55c5dc5bb1743e0d23a6c5c737
MartinMSalas <ingmartinmsalas@gmail.com>
2026-01-23 17:36:55 -0300

    first commit

---
+++ src/main/java/com/esparta/guru_02/Guru02Application.java
@@ -0,0 +1,13 @@
+package com.esparta.guru_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guru02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Guru02Application.class, args);
+	}
+
+}
