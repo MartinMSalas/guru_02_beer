1c4fc6e1e7d8f0ecf9c43039226974d655a3ff43
MartinMSalas <ingmartinmsalas@gmail.com>
2026-02-11 22:02:36 -0300

    test(BeerOrder):setup beforeEach test

--- src/test/java/com/esparta/guru_02/repositories/BeerOrderRepositoryTest.java
+++ src/test/java/com/esparta/guru_02/repositories/BeerOrderRepositoryTest.java
@@ -1,8 +1,15 @@
 package com.esparta.guru_02.repositories;
 
+import com.esparta.guru_02.entities.Beer;
+import com.esparta.guru_02.entities.BeerOrder;
+import com.esparta.guru_02.entities.Customer;
+import com.esparta.guru_02.model.BeerStyle;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +24,36 @@ class BeerOrderRepositoryTest {
 
     @Autowired
     CustomerRepository customerRepository;
+
+    private Customer customer;
+    private Beer beer;
+    private BeerOrder beerOrder;
+
+    @BeforeEach
+    void setUp() {
+        // Create and save a Customer
+        customer = Customer.builder()
+                .customerName("Test Customer")
+                .build();
+        customer = customerRepository.save(customer);
+
+        // Create and save a Beer
+        beer = Beer.builder()
+                .beerName("Test Beer")
+                .beerStyle(BeerStyle.AMERICAN_BARLEYWINE)
+                .upc("123456789012")
+                .price(BigDecimal.valueOf(11.22))
+                .build();
+
+        beer = beerRepository.save(beer);
+
+        // Create and save a BeerOrder
+        beerOrder = BeerOrder.builder()
+                .customer(customer)
+                .customerRef("Test Order Ref")
+                .build();
+        beerOrder = beerOrderRepository.save(beerOrder);
+    }
 
     @Test
     void testBeerOrders(){
